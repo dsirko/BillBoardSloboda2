81a86ec0ffc219307e79c1323d5872e64befc3ba
Anton Zhirkov <an.zhirkov@gmail.com>
2015-01-29 18:43:33 +0200

    CRUD for tags

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -2,6 +2,8 @@ class Advertisement < ActiveRecord::Base
   belongs_to :owner, class_name: User
   belongs_to :adv_category
   has_many :tags, :through => :advertisement_tags
+  has_many :advertisement_tags
+
   has_many :feedbacks
 
 
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,4 @@
 class Tag < ActiveRecord::Base
   has_many :advertisements, :through => :advertisement_tags
+  has_many :advertisement_tags
 end
