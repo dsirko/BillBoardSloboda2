dab1d670fe5da07ae629cc8dddfad7f6c0cb7515
Anton Zhirkov <an.zhirkov@gmail.com>
2015-02-03 10:31:19 +0200

    simple tag cloud

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def tag_cloud(tags, classes)
+    return [] if tags.empty?
+    max_count = tags.sort_by {|e| e[:advertisements.size]}.last.advertisements.size.to_f
+    tags.each do |tag|
+      index = ((tag.advertisements.size.to_f / max_count) * (classes.size - 1))
+      yield tag, classes[index.nan? ? 0 : index.round]
+    end
+  end
 end
