8de12ac6197535e4b96df817c2d11676ef37db54
rostyslav-m <ros55@ukr.net>
2015-02-05 14:37:23 +0200

    add paginate

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -4,13 +4,5 @@ class Advertisement < ActiveRecord::Base
   has_many :tags, :through => :advertisement_tags
   has_many :advertisement_tags
   has_many :feedbacks
-
-  def self.search(search)
-    if search
-      where('text LIKE ?', "%#{search}%") | where('name LIKE ?', "%#{search}%") 
-    else
-      all
-    end
-  end
-
+  scope :search_new, -> (word){ where('LOWER(text) LIKE LOWER(:w) OR LOWER(name) LIKE LOWER(:w)', w: "%#{word}%") if word.present?}
 end
